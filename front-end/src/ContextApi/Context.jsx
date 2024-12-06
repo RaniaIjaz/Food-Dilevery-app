9b38dc07987473d09e8b4c79f26e2ac691a5f20f
RaniaIjaz <142727624+RaniaIjaz@users.noreply.github.com>
2024-12-06 22:37:40 +0500

    Update Context.jsx

--- front-end/src/ContextApi/Context.jsx
+++ front-end/src/ContextApi/Context.jsx
@@ -9,7 +9,8 @@ export const StoreContextProvider = ({ children }) => {
   const [cartItem, setcartItem] = useState({});
   //console.log("cartItem:", cartItem);
 
-  const url = "http://localhost:9000/"
+  //const url = "http://localhost:9000/"
+  const url = "https://food-dilevery-app-back-end.onrender.com"
 
   const [token,setToken] = useState("")
 
